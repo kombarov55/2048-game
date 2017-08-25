483118085a14d9f886cba74ca21a863e9456f210
Nikolay Kombarov <kombarov55@gmail.com>
2017-08-26 01:03:50 +0300

    Лист в мультиплеере

--- src/main/scala/game/swing/MultiplayerPanel.scala
+++ src/main/scala/game/swing/MultiplayerPanel.scala
@@ -1,16 +1,32 @@
 package game.swing
 
-import javax.swing.JButton
+import java.awt.Font
+import javax.swing.border.TitledBorder
+import javax.swing.{BorderFactory, DefaultListModel, JButton, JList, JScrollPane}
 
 import game.Implicits.Function2ActionListener
 
 class MultiplayerPanel extends Panel {
 
+  val participantList = new DefaultListModel[String]
+  val jlist = new JList[String](participantList)
+  jlist.setBounds(125, 75, 250, 250)
+  jlist.setFont(new Font("Arial", Font.PLAIN, 18))
+
+  val border = BorderFactory.createTitledBorder("Выберите соперника")
+  border.setTitleJustification(TitledBorder.CENTER)
+  jlist.setBorder(border)
+
+  participantList.addElement("local")
+  participantList.addElement("local")
+  participantList.addElement("local")
+  participantList.addElement("local")
+
   val watchButton = new JButton("Смотреть")
   val versusButton = new JButton("Играть 1х1")
 
-  watchButton.setBounds(100, 250, 300, 50)
-  versusButton.setBounds(100, 300, 300, 50)
+  watchButton.setBounds(100, 350, 300, 50)
+  versusButton.setBounds(100, 400, 300, 50)
 
 //  watchButton.addActionListener()
 
@@ -18,6 +34,7 @@ class MultiplayerPanel extends Panel {
     setLayout(null)
 
     add(watchButton)
+    add(jlist)
     add(versusButton)
   }
 }
