53d2f5b37ddb15f052b61341fefa94a47a9ad2fb
Nikolay Kombarov <kombarov55@gmail.com>
2017-09-04 11:20:53 +0300

    handler теперь может менять текущее поведение=

--- src/main/scala/game/net/LobbyHandler.scala
+++ src/main/scala/game/net/LobbyHandler.scala
@@ -11,36 +11,37 @@ import game.net.Server.{lobbyHandlers, players}
 
 class LobbyHandler(remote: InetSocketAddress, connection: ActorRef) extends Actor {
 
-  override def receive: Receive = {
+  override def receive: Receive = handleLobby
+
+  def handleLobby: Receive = {
     case Received(data) =>
-      val deserializedData = Serializer.deserialize(data.toArray)
-      println("handler: deserialized=" + deserializedData)
-      self ! deserializedData
+    val deserializedData = Serializer.deserialize(data.toArray)
+    println("handler: deserialized=" + deserializedData)
+    self ! deserializedData
 
     case PeerClosed =>
-      players = players.filterNot { _.address == remote }
-      lobbyHandlers = lobbyHandlers.filterNot { actorRef => actorRef == connection }
-      broadcast(AllPlayers(players))
+    players = players.filterNot { _.address == remote }
+    lobbyHandlers = lobbyHandlers.filterNot { actorRef => actorRef == connection }
+    broadcast(AllPlayers(players))
 
     case ErrorClosed(_) =>
-      players = players.filterNot { _.address == remote }
-      lobbyHandlers = lobbyHandlers.filterNot { actorRef => actorRef == connection }
-      broadcast(AllPlayers(players))
+    players = players.filterNot { _.address == remote }
+    lobbyHandlers = lobbyHandlers.filterNot { actorRef => actorRef == connection }
+    broadcast(AllPlayers(players))
 
     case AddPlayer(name, address) =>
-      val newPlayer = Player(name, address)
-      players = newPlayer :: players
-      broadcast(AllPlayers(players))
+    val newPlayer = Player(name, address)
+    players = newPlayer :: players
+    broadcast(AllPlayers(players))
 
     case AllPlayers =>
-      self ! SendToTheOtherEnd(players)
+    self ! SendToTheOtherEnd(players)
 
     case SendToTheOtherEnd(anything) =>
-      val bytes = Serializer.serialize(anything)
-      connection ! Write(ByteString(bytes))
+    val bytes = Serializer.serialize(anything)
+    connection ! Write(ByteString(bytes))
 
     case unknown: Any => println("handler received unknown message: " + unknown)
-
   }
 
   def  broadcast(message: AnyRef): Unit = {
